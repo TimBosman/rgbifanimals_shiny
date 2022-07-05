6fb82962c29a966c57a70e7fa096b5bfaa26af4c
Daniël <62644989+DvBMolEc@users.noreply.github.com>
2022-07-05 13:13:43 +0200

    Update and clarify

--- 5_Results.R
+++ 5_Results.R
@@ -1,13 +1,16 @@
-straightLine <- read.csv("ShortestPath.csv")
-throughSea <- read.csv("accurate.csv")
+#Input data
+straightLine <- read.csv("Output/ShortestPath.csv")
+throughSea <- read.csv("Output/DistanceOverSea.csv")
 
 test <- merge(throughSea, straightLine, by.y = "Speciesname", by.x = "Specieslist")
 for(item in levels(test$name)){
   test$straight[test$name == item] <- test[test$name == item, item] 
 }
 
-test <- test[,!names(test) %in% c(levels(test$name), "Bodø", "Galway", "Gdynia")]
+#remove unused sampling locations
+test <- test[,!names(test) %in% c(levels(test$name), "Galway", "Gdynia")]
 
+#plot
 ggplot(test)+
   geom_point(aes(x = straight , y = distance), col = "green") +
   geom_abline()
