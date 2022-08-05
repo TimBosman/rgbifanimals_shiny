a14427b4d0121f41f7671c6c2de75cc4d7834aa3
TimBosman <50997599+TimBosman@users.noreply.github.com>
2022-08-05 22:10:25 +0200

    The map only plots the point where sequences are found
    Close #3

--- functions.R
+++ functions.R
@@ -9,7 +9,8 @@ plotMap <- function(df, locations, occurrence, size = ""){
     theme_void() + 
     geom_hex(data = occurrence, aes(x = Longitude, y = Latitude), alpha = 0.8, col = "White") +
     scale_fill_gradient(low = "#8CDA8F", high = "#0D1F0B") +
-    geom_point(data = locations, aes(x = Longitude, y = Latitude, col = Observatory.ID), pch = 16, size = 5)+
+    geom_point(data = locations[locations$Observatory.ID %in% names(df)[df[1,]>0],], 
+               aes(x = Longitude, y = Latitude, col = Observatory.ID), pch = 16, size = 5)+
     scale_color_manual("Sample Location", values = locCols) 
     if(size == "europe"){
       plot <- plot + coord_cartesian(xlim = c(-25, 60), ylim = c(35,70))
