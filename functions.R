397b57d74be308de5470e735fd6708bfbbb68102
TimBosman <50997599+TimBosman@users.noreply.github.com>
2022-12-10 13:58:53 +0100

    Cleaned the code

--- functions.R
+++ functions.R
@@ -1,22 +1,26 @@
 ################################ Load Packages #################################
 library("ggplot2")
 library("leaflet")
+library("tidyr")
 
 ################################## Functions ###################################
 
-plotBar <- function(df) {
+plot_bar <- function(df, loc_cols) {
   plot <- ggplot(pivot_longer(df, names(df)[-1])) +
     theme(axis.text.x = element_text(angle = 15)) +
     geom_bar(aes(x = name, y = value, fill = name), stat = "identity") +
-    scale_fill_manual("Sample Location", values = locCols)
+    scale_fill_manual("Sample Location", values = loc_cols)
   return(plot)
 }
 
-plotLeaflet <- function(df, locations, occurrence) {
-  aicon = awesomeIcons(markerColor = locCols) 
+plot_leaflet <- function(df, locations, occurrence, loc_cols) {
+  locations <- locations[locations$Observatory.ID %in% names(df)[df[1, ] > 0], ]
+  aicon <- awesomeIcons(markerColor = loc_cols)
   leaflet() %>%
     addProviderTiles(providers$Stamen.TonerLite,
     options = providerTileOptions(noWrap = TRUE)) %>%
-      addAwesomeMarkers(data = locations[locations$Observatory.ID %in% names(df)[df[1,]>0],], ~Longitude, ~Latitude, label = ~Observatory.ID, icon = aicon) %>%
-      addMarkers(data = occurrence, ~Longitude, ~Latitude, clusterOptions = markerClusterOptions())
+      addAwesomeMarkers(data = locations, ~Longitude, ~Latitude,
+        label = ~Observatory.ID, icon = aicon) %>%
+      addMarkers(data = occurrence, ~Longitude, ~Latitude,
+        clusterOptions = markerClusterOptions())
 }
--- shiny.R
+++ shiny.R
@@ -8,14 +8,12 @@ library("leaflet")
 # library("rentrez")
 library("RColorBrewer")
 
-#setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 source("functions.R")
 df <- readRDS("inputs/BOLDigger_Species_Location.rds")
 locations <- read.csv("inputs/Coordinates.csv")
 
-# locations$Observatory.ID
-locCols <- brewer.pal(nrow(locations), "Set3")
-names(locCols) <- locations$Observatory.ID
+loc_cols <- brewer.pal(nrow(locations), "Set3")
+names(loc_cols) <- locations$Observatory.ID
 
 ui <- pageWithSidebar(
   # App title ----
@@ -23,9 +21,8 @@ ui <- pageWithSidebar(
   # Sidebar panel for inputs ----
   sidebarPanel(
     selectInput("species", "Species", df$Specieslist),
-    # radioButtons("line", "Show shortest path through sea", c("No" = FALSE, "Yes" = TRUE)),
-    # radioButtons("visualization", "Show occurrences as", c("Points" = geom_point, "Hexagons" = geom_hex),
-    # radioButtons("allLoc", "Show all sample locations",c("Yes" = TRUE, "No" = FALSE)),
+    # radioButtons("line", "Show shortest path through sea",
+      # c("No" = FALSE, "Yes" = TRUE)),
     width = 2,
     tableOutput("sptable")
   ),
@@ -36,27 +33,27 @@ ui <- pageWithSidebar(
     width = 10
   )
 )
-server <- function(input, output){
+server <- function(input, output) {
   output$species <- renderText(input$species)
-  dfSp <- eventReactive(input$species, df[df$Specieslist == input$species, ])
-  infoSp <- eventReactive(input$species, t(name_backbone(input$species)[,c("usageKey", "scientificName", "kingdom", "phylum", "order", "family", "class")]))
-  # NCBI <- eventReactive(input$species, entrez_search(db="taxonomy", term=paste0("(", input$species, "[ORGN]) AND Species[RANK]"))$ids)
-  # infoSp <- eventReactive(input$species, rbind(infoSp, data.frame("NCBI ID" = NCBI)))
+  df_sp <- eventReactive(input$species, df[df$Specieslist == input$species, ])
+  info_cols <- c("usageKey", "scientificName", "kingdom", "phylum", "order",
+    "family", "class")
+  info_sp <- eventReactive(input$species,
+    t(name_backbone(input$species)[, info_cols]))
+  # NCBI <- eventReactive(input$species, entrez_search(db="taxonomy",
+  #   term=paste0("(", input$species, "[ORGN]) AND Species[RANK]"))$ids)
+  # info_sp <- eventReactive(input$species,
+  #   rbind(infoSp, data.frame("NCBI ID" = NCBI)))
   occurrence <- eventReactive(input$species, {
     read.csv(paste0("OccurrenceData/", input$species, ".csv"))
   })
-  output$barplot <- renderPlot(plotBar(dfSp()))
-  output$sptable <- renderTable(infoSp(), rownames = TRUE, colnames = FALSE)
-  output$mymap <- renderLeaflet(plotLeaflet(dfSp(), locations, occurrence()))
+  output$barplot <- renderPlot(plot_bar(df_sp(), loc_cols))
+  output$sptable <- renderTable(info_sp(), rownames = TRUE, colnames = FALSE)
+  output$mymap <- renderLeaflet(plot_leaflet(df_sp(), locations, occurrence(),
+    loc_cols))
 }
 
 
 options(shiny.host = "0.0.0.0")
 options(shiny.port = 80)
 shinyApp(ui, server)
-
-
-# species = "Homo sapiens"
-# infoSp <- t(name_backbone(species)[,c("usageKey", "scientificName", "kingdom", "phylum", "order", "family", "class")])
-# NCBI <- eventReactive(input$species, entrez_search(db="taxonomy", term=paste0("(", input$species, "[ORGN]) AND Species[RANK]"))$ids
-# infoSp <- rbind(infoSp, data.frame("NCBI ID" = NCBI))
